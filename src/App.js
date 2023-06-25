b48797bcc3cb7e552830695152e3d21d86fe8a29
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:06:24 -0400

    add News link and nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import GameList from "./components/GameList";
 import GameForm from "./components/GameForm";
 import Home from "./components/Home";
+import News from "./components/News";
 
 function App() {
   const [games, setGames] = useState([]);
@@ -17,13 +18,15 @@ function App() {
       <Router>
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/game-search">Game Search</Link>
+          <Link to="/news">News</Link>
           <Link to="/add-game">Add Game</Link>
+          <Link to="/game-search">Game Search</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Home games={games} />} />
-          <Route path="/game-search" element={<GameList games={games} />} />
+          <Route path="/news" element={<News />} />
           <Route path="/add-game" element={<GameForm addGame={addGame} />} />
+          <Route path="/game-search" element={<GameList games={games} />} />
         </Routes>
       </Router>
     </div>
