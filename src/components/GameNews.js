50131bfca5a40aec51a487413c90a134be3b345c
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:36:46 -0400

    add Pagination component to GameNew component

--- src/components/GameNews.js
+++ src/components/GameNews.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Pagination from "./Pagination";
 
 const News = () => {
   const [news, setNews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const newsPerPage = 10;
 
   useEffect(() => {
     fetchNews();
@@ -19,18 +22,41 @@ const News = () => {
       });
   };
 
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const indexOfLastNews = currentPage * newsPerPage;
+  const indexOfFirstNews = indexOfLastNews - newsPerPage;
+  const currentNews = news.slice(indexOfFirstNews, indexOfLastNews);
+  const totalPages = Math.ceil(news.length / newsPerPage);
+
   return (
     <div>
       <h2>Game News</h2>
-      {news.map((currentNews) => (
+      {currentNews.map((currentNews) => (
         <div key={currentNews.id}>
-          <a href={currentNews.article_url} target="_blank">
+          <a
+            href={currentNews.article_url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={currentNews.main_image} alt="game thumbnail" />
           </a>
           <h3>{currentNews.title}</h3>
           <p>{currentNews.short_description}</p>
         </div>
       ))}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePrevPage={handlePrevPage}
+        handleNextPage={handleNextPage}
+      />
     </div>
   );
 };
