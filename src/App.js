8fe5fe99c22d3f42d550bdfbb34e85d166dc4ca1
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 06:27:19 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import "./App.css";
+// App.js
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import GameList from "./components/GameList";
 import GameForm from "./components/GameForm";
-import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [games, setGames] = useState([]);
+
+  const addGame = (newGame) => {
+    setGames([...games, newGame]);
+  };
+
   return (
     <div className="App">
       <Router>
@@ -13,8 +19,8 @@ function App() {
           <Link to="/add-game">Add Game</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<GameList />} />
-          <Route path="/add-game" element={<GameForm />} />
+          <Route path="/" element={<GameList games={games} />} />
+          <Route path="/add-game" element={<GameForm addGame={addGame} />} />
         </Routes>
       </Router>
     </div>
