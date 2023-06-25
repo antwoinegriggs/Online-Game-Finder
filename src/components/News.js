7f35ef17abdc95bcb48744bbe3b60aab0cdd4b63
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:15:33 -0400

    add fetch to GameNews component

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from "react";
 
 const News = () => {
   const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = () => {
+    fetch("https://www.mmobomb.com/api1/latestnews")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("GameNews GET Success", data);
+        setNews(data);
+      })
+      .catch((error) => {
+        console.error("GameNews GET Error:", error);
+      });
+  };
 };
 
 export default News;
