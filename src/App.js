a20a1ef83d04e5849b4357035f5832cf7bf38e5b
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-24 23:50:29 -0400

    render test add routes and nav without NavBar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 import GameList from "./components/GameList";
 import GameForm from "./components/GameForm";
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <GameForm />
-      <GameList />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Game List</Link>
+            </li>
+            <li>
+              <Link to="/add-game">Add Game</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<GameList />} />
+          <Route path="/add-game" element={<GameForm />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
