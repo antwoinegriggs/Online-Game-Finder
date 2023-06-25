cbaed6d0c03804c40ed17b02f2fa9896417dd908
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-24 23:38:37 -0400

    render test GameForm.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import GameList from "./components/GameList";
+import GameForm from "./components/GameForm";
 
 function App() {
   return (
     <div className="App">
+      <GameForm />
       <GameList />
     </div>
   );
