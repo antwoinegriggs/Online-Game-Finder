8119f44a5a4ffeb4cffd1c6d281456e616de83eb
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:28:34 -0400

    update GameItem.js

--- src/components/GameItem.js
+++ src/components/GameItem.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-const GameItem = ({ game }) => {};
+const GameItem = ({ game }) => {
+  return (
+    <div>
+      <img src={game.thumbnail} alt="game thumbnail" />
+      <h3>{game.title}</h3>
+      <p>{game.genre}</p>
+    </div>
+  );
+};
 
 export default GameItem;
