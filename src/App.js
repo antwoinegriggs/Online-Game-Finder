7eed31741a9fb776ad29e116dfb2f0964c458f80
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-24 23:43:49 -0400

    update NavBar BrowserRouter Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import GameList from "./components/GameList";
 import GameForm from "./components/GameForm";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
