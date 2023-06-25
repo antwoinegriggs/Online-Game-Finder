30b07ebc98fc0cf99e8cb45571c837742054893c
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:00:21 -0400

    add Home link and nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import GameList from "./components/GameList";
 import GameForm from "./components/GameForm";
+import Home from "./components/Home";
 
 function App() {
   const [games, setGames] = useState([]);
@@ -15,11 +16,13 @@ function App() {
     <div className="App">
       <Router>
         <nav>
-          <Link to="/">Game List</Link>
+          <Link to="/">Home</Link>
+          <Link to="/game-search">Game Search</Link>
           <Link to="/add-game">Add Game</Link>
         </nav>
         <Routes>
-          <Route path="/" element={<GameList games={games} />} />
+          <Route path="/" element={<Home games={games} />} />
+          <Route path="/game-search" element={<GameList games={games} />} />
           <Route path="/add-game" element={<GameForm addGame={addGame} />} />
         </Routes>
       </Router>
