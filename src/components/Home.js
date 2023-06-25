15dea6f82acd13f53a04e81c10fadfbe8e5ba599
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:44:32 -0400

    add lastet 5 news to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,23 @@
 import React, { useState, useEffect } from "react";
 
-const Home = () => {};
+const Home = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetchNews();
+  }, []);
+
+  const fetchNews = () => {
+    fetch("https://www.mmobomb.com/api1/latestnews")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("LatestNews GET Success", data);
+        setNews(data.slice(0, 5));
+      })
+      .catch((error) => {
+        console.error("LatestNews GET Error:", error);
+      });
+  };
+};
 
 export default Home;
