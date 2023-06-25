70a7b1c8cc0e9dddd6409a783f9480d17b4bcbff
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 09:07:14 -0400

    update GameList.js add pagination

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const GameList = () => {
   const [games, setGames] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const gamesPerPage = 25;
 
   useEffect(() => {
     fetchGames();
   }, []);
 
   const fetchGames = () => {
-    fetch("http://localhost:3001/games/")
+    fetch("http://localhost:3001/games")
       .then((response) => response.json())
       .then((data) => {
         console.log("GameList GET Success");
@@ -19,18 +22,41 @@ const GameList = () => {
       });
   };
 
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  // Calculate the index range of games to be rendered for the current page
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+
   return (
     <div>
       <h2>Game List</h2>
-      {games.map((game) => (
+      {currentGames.map((game) => (
         <div key={game.id}>
-          <a href={game.game_url}>
-            <img src={game.thumbnail} alt="game thumbnail" />
-          </a>
+          <img src={game.thumbnail} alt="game thumbnail" />
           <h3>{game.title}</h3>
           <p>{game.genre}</p>
         </div>
       ))}
+      <div>
+        {currentPage > 1 && (
+          <Link to="#" onClick={handlePrevPage}>
+            Previous Page
+          </Link>
+        )}
+        {currentPage < Math.ceil(games.length / gamesPerPage) && (
+          <Link to="#" onClick={handleNextPage}>
+            Next Page
+          </Link>
+        )}
+      </div>
     </div>
   );
 };
