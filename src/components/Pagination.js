aea9b65c9ad46650430d3c12ca4f0330121eac65
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-25 10:29:11 -0400

    update Pagination.js

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Pagination = () => {};
+const Pagination = ({
+  currentPage,
+  totalPages,
+  handlePrevPage,
+  handleNextPage,
+}) => {
+  return (
+    <div>
+      {currentPage > 1 && (
+        <Link to="#" onClick={handlePrevPage}>
+          Previous Page
+        </Link>
+      )}
+      {currentPage < totalPages && (
+        <Link to="#" onClick={handleNextPage}>
+          Next Page
+        </Link>
+      )}
+    </div>
+  );
+};
 
 export default Pagination;
